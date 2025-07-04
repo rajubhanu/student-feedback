4ddee814e8a0466da9dddbf27d6f4edb6b5ea0e0
rajubhanu <33171357+rajubhanu@users.noreply.github.com>
2025-07-04 14:46:19 +0530

    Update feedbacks.sql

--- feedbacks.sql
+++ feedbacks.sql
@@ -2,7 +2,10 @@ CREATE TABLE feedbacks (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(100),
   rollno VARCHAR(20),
+  year VARCHAR(20),
+  semester VARCHAR(20),
   subject VARCHAR(100),
   rating INT,
-  comments TEXT
+  comments TEXT,
+  submitted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
